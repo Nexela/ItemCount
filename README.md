b630adabd5ad15796cd388a659253d4db877ccbf
Nicholas Dunn <nexela@gmail.com>
2016-07-24 09:37:48 -0400

    Changes to use held stack change event.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ItemCount
+
+Port of the Item Count mod by ThaPear
+
 Will display a count of all of the items in your inventory that you have in your hand.
+
+v0.0.4 - Removed onTick event and using new .13 on_player_cursor_stack_changed event
+v0.0.3 - Changes for Factorio.13
--- control.lua
+++ control.lua
@@ -1,24 +1,22 @@
 local guiOpen = false
 
-script.on_event(defines.events.on_tick, function(event) OnTick(event) end)
+script.on_event(defines.events.on_player_cursor_stack_changed, function(event) CheckStack(event) end)
 
 local oldStack = nil
 local totalCount = 0
-function OnTick(event)
-	for _, player in pairs(game.players) do
-		CheckStack(player)
-	end
-end
 
-function CheckStack(player)
-	if player.cursor_stack.valid_for_read then
+function CheckStack(event)
+	local player=game.players[event.player_index]
+	if player and player.cursor_stack.valid_for_read then
 		OpenGUI(player)
 		CalculateTotal(player, player.cursor_stack)
 		SetGUIText(player, totalCount)
 	else
 		CloseGUI(player)
 	end
+	--]]
 end
+
 
 function CalculateTotal(player, stack)
 	totalCount = stack.count
@@ -34,11 +32,13 @@ function SetGUIText(player, text)
 		player.gui.center.itemcount.caption = text
 	end
 end
+
 function OpenGUI(player)
 	if not player.gui.center.itemcount then
 		player.gui.center.add{type="label", name="itemcount", caption="100000", direction = "vertical"}
 	end
 end
+
 function CloseGUI(player)
 	if player.gui.center.itemcount then
 		player.gui.center.itemcount.destroy()
