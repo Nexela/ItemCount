fd3d7398e33a192f7456a9def94d9afd913fbef2
Nicholas Dunn <nexela@gmail.com>
2016-09-09 22:35:45 -0400

    Vehicle item count, and sleeker code polish

--- control.lua
+++ control.lua
@@ -1,46 +1,45 @@
-local guiOpen = false
-
-script.on_event(defines.events.on_player_cursor_stack_changed, function(event) CheckStack(event) end)
-
-local oldStack = nil
-local totalCount = 0
-
-function CheckStack(event)
-	local player=game.players[event.player_index]
-	if player and player.cursor_stack.valid_for_read then
-		OpenGUI(player)
-		CalculateTotal(player, player.cursor_stack)
-		SetGUIText(player, totalCount)
-	else
-		CloseGUI(player)
+local function SetGUIText(player, count, vehicle_count)
+	local text = tostring(count)
+	if vehicle_count > 0 then
+		text = text .. "  (".. tostring(vehicle_count) ..")"
 	end
-	--]]
-end
-
-
-function CalculateTotal(player, stack)
-	totalCount = stack.count
-	local inventory = player.get_inventory(defines.inventory.player_quickbar)
-	totalCount = totalCount + inventory.get_item_count(stack.name)
-	inventory = player.get_inventory(defines.inventory.player_main)
-	totalCount = totalCount + inventory.get_item_count(stack.name)
-end
-
-function SetGUIText(player, text)
 	if player.gui.center.itemcount then
 		player.gui.center.itemcount.style.font = "default-bold"
 		player.gui.center.itemcount.caption = text
 	end
 end
 
-function OpenGUI(player)
+local function OpenGUI(player)
 	if not player.gui.center.itemcount then
-		player.gui.center.add{type="label", name="itemcount", caption="100000", direction = "vertical"}
+		player.gui.center.add{type="label", name="itemcount", caption="0", direction = "vertical"}
+		player.gui.center.itemcount.style.minimal_width=32
 	end
 end
 
-function CloseGUI(player)
+local function CloseGUI(player)
 	if player.gui.center.itemcount then
 		player.gui.center.itemcount.destroy()
 	end
 end
+
+local function CalculateTotal(player, item_name, vehicle)
+	local inventory_count = player.get_item_count(item_name)
+	local vehicle_count = 0
+	if vehicle and vehicle.get_inventory(defines.inventory.car_trunk) then
+		vehicle_count = vehicle.get_inventory(defines.inventory.car_trunk).get_item_count(item_name)
+	end
+	return inventory_count, vehicle_count
+end
+
+local function CheckStack(event)
+	local player=game.players[event.player_index]
+	if player and player.cursor_stack.valid_for_read then
+		OpenGUI(player)
+		SetGUIText(player, CalculateTotal(player, player.cursor_stack.name, player.vehicle))
+	else
+		CloseGUI(player)
+	end
+	--]]
+end
+local events = {defines.events.on_player_cursor_stack_changed, defines.events.on_player_driving_changed_state}
+script.on_event(events, function(event) CheckStack(event) end)
